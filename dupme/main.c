a386ef224bb55dfb5462358f0ddcacef8facbde5
warrior <arseniy1993@mail.ru>
2013-04-02 17:29:06 +0000

    added print

--- dupme/main.c
+++ dupme/main.c
@@ -1,1 +1,25 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
+char *buffer;
+int buffer_size;
+
+void print(const char *buf, size_t count, size_t times)
+{
+    int written;
+    int i;
+    for (i = 0; i < times; ++i)
+    {
+        written = 0;
+        while (written < count)
+        {
+            wittern += write(stdout, buf + written, count - written);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
